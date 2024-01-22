31421a25c0c794e9f3df31c0ee4611d222567136
pawx10 <arroyopablo2004@gmail.com>
2024-01-22 17:44:07 +0100

    commit

--- deploy-delete/borrar.sh
+++ deploy-delete/borrar.sh
@@ -1,8 +1,9 @@
 
 
-export AWS_ACCESS_KEY_ID=
-export AWS_SECRET_ACCESS_KEY=
-export AWS_SESSION_TOKEN=
+export AWS_ACCESS_KEY_ID=ASIAVS4F2WJD4MXY5H67
+export AWS_SECRET_ACCESS_KEY=oEcVXt27c5vmSdVlcb5QkGencPkhWwdcezLv76+e
+
+export AWS_SESSION_TOKEN==FwoGZXIvYXdzEKL//////////wEaDOu31dqucPl8jW4fRCLGATHBSS8MojcaB5zeZe9U6s91aNgGBEmBYbHeQ5GIHR4qrmzxfp4Tf4n2LXdzC2G7UXNHQk3D+obY7qTjo4rCswsSxqK8Frx4im0o1NVskDVZTN8KLmgM4RVuHKV6KG0BnoPL4jhCSGGM07xzQhuHM4RE8cqbw/gSKYPa1ZhduaDlu8T/kehXiGrSRk1AWA+ydjuqaWiCgqQXqdNsVxYPXnA//Qt+9di3eiHtJtsm+xSrC6jLyuGwL9OP+AMkICdsRATeK5Be+SjRtLqtBjItDpbqutO9i5x7B764FcGFFiYpd7Xoh6Eknlfkg/wHRp8HhheLqE7+4hvFEK2H
 export AWS_DEFAULT_REGION=us-east-1
 aws cloudformation delete-stack \
 --stack-name tomcat \
