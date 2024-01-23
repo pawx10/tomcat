d07824071c789b889ceddcd6465669fa446a1cf1
pawx10 <arroyopablo2004@gmail.com>
2024-01-23 20:56:08 +0100

    commit

--- deploy-delete/deploy.sh
+++ deploy-delete/deploy.sh
@@ -3,7 +3,7 @@ export AWS_ACCESS_KEY_ID=
 export AWS_SECRET_ACCESS_KEY=
 
 export AWS_SESSION_TOKEN=
-export AWS_DEFAULT_REGION=cd AWS_ACCESS_KEY_ID
+export AWS_DEFAULT_REGION=us-east-1
 aws cloudformation deploy \
 --template-file tomcat/yaml/main.yml \
 --stack-name "tomcat" \
