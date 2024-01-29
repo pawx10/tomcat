6e4bcd7ef2549305aa207b7f855e677bed1e31c7
pawx10 <arroyopablo2004@gmail.com>
2024-01-29 17:06:36 +0100

    commit

--- deploy-delete/deploy.sh
+++ deploy-delete/deploy.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
+
+apt update -y && apt upgrade -y && apt auto-remove -y
+
+apt install awscli -y
+
 export AWS_ACCESS_KEY_ID=
 export AWS_SECRET_ACCESS_KEY=
-
 export AWS_SESSION_TOKEN=
 export AWS_DEFAULT_REGION=us-east-1
+
+STACK_NAME=tomcat
+EC2_INSTANCE_TYPE=t2.medium
+
 aws cloudformation deploy \
---template-file tomcat/yaml/main.yml \
---stack-name "tomcat" \
---capabilities CAPABILITY_NAMED_IAM
+--template-file apache-tomcat-daweb/stack-yaml/main.yml \
+--stack-name $STACK_NAME \
+--capabilities CAPABILITY_NAMED_IAM \
+--no-fail-on-empty-changeset \
+--parameter-override InstanceType=$EC2_INSTANCE_TYPE
